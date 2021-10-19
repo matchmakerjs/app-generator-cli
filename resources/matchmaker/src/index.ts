00da4b42a7a7bd16d778a48df54b4c1ab4129f08
Olaleye Afolabi <olaleyeone@gmail.com>
2021-10-19 05:37:47 +0100

    app server startup updated

--- resources/matchmaker/src/index.ts
+++ resources/matchmaker/src/index.ts
@@ -1,7 +1,8 @@
 import { createContainer } from '@matchmakerjs/di';
-import { startServerWithGracefulShutdown } from '@matchmakerjs/matchmaker';
+import { addGracefulShutdown, startServer } from '@matchmakerjs/matchmaker';
 import { RemoteKeyAccessClaimsValidator, SecureRequestListener } from '@matchmakerjs/matchmaker-security';
 import axios from 'axios';
+import * as http from 'http';
 import argumentListResolver from './conf/argument-list-resolver';
 import router from './conf/router';
 import validator from './conf/validator';
@@ -9,12 +10,11 @@ import validator from './conf/validator';
 const [container, cleanUp] = createContainer({
     modules: [],
 });
-startServerWithGracefulShutdown(
-    SecureRequestListener(router, {
-        container,
-        argumentListResolver,
-        validator,
-        accessClaimsResolver: RemoteKeyAccessClaimsValidator(process.env.WEB_KEY_URL, axios)
-    }),
-    cleanUp,
-);
+const server = http.createServer(SecureRequestListener(router, {
+    container,
+    argumentListResolver,
+    validator,
+    accessClaimsResolver: RemoteKeyAccessClaimsValidator(process.env.WEB_KEY_URL, axios)
+}));
+addGracefulShutdown(server, cleanUp);
+startServer(server);
